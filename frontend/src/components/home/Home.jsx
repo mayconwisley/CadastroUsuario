6368f4ff48ecb58aa954784052982b1b162043c5
mayconwisley <maycon.wisley@outlook.com>
2021-12-05 11:22:18 -0300

    Curso Web Moderno

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Inicio" subtitle="Segundo Projeto">
+        <div className="display-4">Bem Vindo</div>
+        <hr />
+        <p className='mb-0'>
+            Sistema desenvolvimento para exemplificar a contrução de um Cadastro desenvolvido em React!
+        </p>
+    </Main>
